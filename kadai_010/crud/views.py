e8101b3cc69df41daa3f4b6fc03a391f3877c91c
yuk199 <y.funakoshi@and-relief.com>
2024-11-18 23:31:47 +0900

    課題提出用ファイルのアップロード

---
+++ kadai_010/crud/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Product
+from django.urls import reverse_lazy
+
+# Create your views here.
+class TopView(TemplateView):
+  template_name = "top.html"
+
+class ProductListView(ListView):
+  model = Product
+  template_name = "list.html"
+  paginate_by = 3
+
+class ProductCreateView(CreateView):
+  model = Product
+  fields = '__all__'
+
+class ProductUpdateView(UpdateView):
+  model = Product
+  fields = '__all__'
+  template_name_suffix = '_update_form'
+
+class ProductDeleteView(DeleteView):
+  model = Product
+  success_url = reverse_lazy('list')
+
+class ProductDetailView(DetailView):
+  model = Product
